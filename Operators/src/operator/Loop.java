3a212633b6a0d12809e8205202a3c34d8cb5a74b
Anusha Gupta <anusha.gupta@cognizant.com>
2018-10-26 21:31:04 +0530

    first commit

---
+++ Operators/src/operator/Loop.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		while (a > 0) {
+			System.out.println(a);
+			a--;
+		}
+	}
+
+}
---
+++ project1/src/project/Main.java
@@ -0,0 +1,33 @@
+package project;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*@SuppressWarnings("resource")
+		String fast = "Fast";
+		Scanner sc = new Scanner(System.in);
+		boolean powerplay = sc.nextBoolean();
+		String ball = sc.next();
+		if(powerplay) {
+			System.out.println("Yes");
+		}
+		else {
+			if (ball == fast) {
+				System.out.println("Yes");
+			}
+			else {
+				System.out.println("No");
+			}
+		}
+		*/
+		int x=034;
+		System.out.println(x);
+		int y=12;
+		int ans=x+y;
+		System.out.println(ans);	
+
+	}
+
+}
